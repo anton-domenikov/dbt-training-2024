d920d81e8ac9ad3475db3a346e366c2055b09684
Anton Domenikov <anton.domenikov@gmail.com>
2024-03-11 12:12:28 +0000

    added fact table and more models

--- models/marts/core/dim_customers.sql
+++ models/marts/core/dim_customers.sql
@@ -3,7 +3,7 @@ with customers as (
 ),
 
 orders as (
-    select * from {{ ref('stg_orders') }}
+    select * from {{ ref('fct_orders') }}
 ),
 
 customer_orders as (
@@ -13,7 +13,8 @@ customer_orders as (
 
         min(order_date) as first_order_date,
         max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
+        count(order_id) as number_of_orders,
+        sum(amount) as lifetime_value
 
     from orders
 
@@ -30,6 +31,7 @@ final as (
         customers.last_name,
         customer_orders.first_order_date,
         customer_orders.most_recent_order_date,
+        customer_orders.lifetime_value,
         coalesce(customer_orders.number_of_orders, 0) as number_of_orders
 
     from customers

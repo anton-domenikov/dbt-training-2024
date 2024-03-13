2e3dab3e11daff2155f909a4b6e109bd980c0ca4
Anton Domenikov <anton.domenikov@gmail.com>
2024-03-13 16:09:58 +0000

    moved out a join that was calculating amount spend in the final select statement, working fine

--- models/marts/core/refactorED_customers_orders.sql
+++ models/marts/core/refactorED_customers_orders.sql
@@ -8,34 +8,37 @@ customer_orders as (
     select * from {{ ref('int_customer_orders_refactoring') }}
 ),
 
+clv_aggregation as (
+    select paid_orders.order_id, sum(t2.total_amount_paid) as clv_bad
+    from paid_orders
+    left join
+        paid_orders t2
+        on paid_orders.customer_id = t2.customer_id
+        and paid_orders.order_id >= t2.order_id
+    group by 1
+    order by paid_orders.order_id
+),
+
 -- final CTE
 final as (
 
     select
-        p.*,
-        row_number() over (order by p.order_id) as transaction_seq,
+        paid_orders.*,
+
+        row_number() over (order by paid_orders.order_id) as transaction_seq,
         row_number() over (
-            partition by customer_id order by p.order_id
+            partition by customer_id order by paid_orders.order_id
         ) as customer_sales_seq,
         case
-            when c.first_order_date = p.order_placed_at then 'new' else 'return'
+            when customer_orders.first_order_date = paid_orders.order_placed_at then 'new' else 'return'
         end as nvsr,
         x.clv_bad as customer_lifetime_value,
-        c.first_order_date as fdos
-    from paid_orders p
-    left join customer_orders as c using (customer_id)
+        customer_orders.first_order_date as fdos
+    from paid_orders
+    left join customer_orders using (customer_id)
     left outer join
-        (
-            select p.order_id, sum(t2.total_amount_paid) as clv_bad
-            from paid_orders p
-            left join
-                paid_orders t2
-                on p.customer_id = t2.customer_id
-                and p.order_id >= t2.order_id
-            group by 1
-            order by p.order_id
-        ) x
-        on x.order_id = p.order_id
+        clv_aggregation x
+        on x.order_id = paid_orders.order_id
     order by order_id
 )
 
